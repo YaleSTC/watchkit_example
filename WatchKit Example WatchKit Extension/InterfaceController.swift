5b20a14d0d2e9a68d8c472cd17d5673d36e489e3
Lee Danilek <lee.danilek@yale.edu>
2016-10-07 16:47:10 -0400

    Initial Commit

---
+++ WatchKit Example WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  WatchKit Example WatchKit Extension
+//
+//  Created by Lee on 10/7/16.
+//  Copyright © 2016 Yale SDMP. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
