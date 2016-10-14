bead41888513818b7c4dbf4e8ddf798674fe1faa
shanelleroman <shanelle.roman@yale.edu>
2016-10-14 18:33:41 -0400

    working on the callback functions

--- WatchKit Example WatchKit App/locationModule.swift
+++ WatchKit Example WatchKit App/locationModule.swift
@@ -13,6 +13,9 @@ class LocationModule: NSObject, CLLocationManagerDelegate {
     // utilizes lazy variables
     lazy var locationManager: CLLocationManager = self.manager()
     var currentLocation: CLLocation?;
+    var stops = [Int: CLLocationCoordinate2D] ()
+    var callback: (([Int]) ->Void)
+
     
     
      func manager() -> CLLocationManager {
@@ -23,7 +26,49 @@ class LocationModule: NSObject, CLLocationManagerDelegate {
         return myManager
     }
 
+    // TODO: 
+    /* 1. write a function filter(stops) declaration: func filter ([String: CLL2D coordinates]) -> [Int]
+        takes in a dictionary of the stopID and CLL2D coordinates, filters them and finds the nearest Stops and returning them in an integer array
+        2. beginUpdaates function that utilizes a call back that takes the same dictionary that filter took in, and an completion handler - it stores the value of dictionary and the value of the callback
+            declaration: func beginUpdates(dict: [int: CLL2D], completionHandler: ([int])->Void) {
+            self.stops = dict
+            self.callback = completionHandler
+        3. finally in the didUpdate function - use callback(filter(stops))
+    // takes in an NSDictionary that pairs Stop Ids with CLL2D coordinates
+    // filters by the current location
+    // returns the closest location
+  /*  func findNearbyStops(allStops: [String: CLLocationCoordinate2D])-> [String] {
+// filtering can't be done here
+     // helper filter Stops method - 
+     didUpdate
+    } */ */
     
+    // returns a String array of the nearest stops
+    func calculateShortestDistance(currentLocation: CLLocationCoordinate2D, dict: [String: CLLocationCoordinate2D])->[String] {
+        
+        // check if have done this calculation before - returns nearest bus stops
+        // given a current location
+        // is it possible to do a memoizedValues - there isn't really a point unless the key is for your current location
+        /*
+        let memoizedValue = memoizedValues[currentLocation]
+        if memoizedValue != nil {
+            return memoizedValue
+        } */
+        
+        var shortestDistance: CLLocationDistance? = nil
+        if dict.count > 0 {
+
+        }
+        
+        
+    }
+    
+    func filter(dict: [String: CLLocationCoordinate2D])->[Int]
+    {
+        
+    }
+    
+ 
  
    func startUpdatingLocation() {
         self.locationManager.startUpdatingLocation()
@@ -50,8 +95,9 @@ class LocationModule: NSObject, CLLocationManagerDelegate {
     }
     
     // location delegate stuff
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]){
         self.currentLocation = locations.last!
+        callback(filter(stops))
     }
     
 }
