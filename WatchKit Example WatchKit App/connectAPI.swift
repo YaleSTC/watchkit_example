32e20449c245a6c4604502d5c6c575b4a4527d1f
shanelleroman <shanelle.roman@yale.edu>
2016-10-13 22:47:05 -0400

    Attempted to get routes

--- WatchKit Example WatchKit App/connectAPI.swift
+++ WatchKit Example WatchKit App/connectAPI.swift
@@ -7,9 +7,50 @@
 //
 
 import UIKit
+import Foundation
+
 
 
 class connectAPI: NSObject  {
+    
+    public func getRoutes( )->Void
+    {
+        // configure URL session
+        let session = URLSession(configuration: URLSessionConfiguration.default)
+        
+        // create NSURL request
+        let urlString = "https://transloc-api-1-2.p.mashape.com/routes.json?agencies=yale&geo_area=41.3%2C-72.9%7C5000"
+        let url = URL(fileURLWithPath: urlString)
+        var request = NSMutableURLRequest(url: url)
+        
+        // set method type and body
+        request.httpMethod = "GET"
+        request.setValue("application/json", forHTTPHeaderField: "accept")
+        request.setValue("tbZ4YpVZyMmshj9yNjoXCsqXr3k9p1R36r5jsnH541Ejy6Qk6y", forHTTPHeaderField: "X-Mashape-Key")
+        
+        let task = session.dataTask(with: request as URLRequest, completionHandler: {(data, response, error) in
+            
+            // create the returning JSON data object
+            if (data == nil)
+            {
+                // NO IDEA WHAT TO DO NEXT
+                completionnil
+            }
+            else
+            {
+               let responseJSON =  try? JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers)
+                return responseJSON
+
+            }
+            
+        });
+        
+        task.resume()
+
+ 
+    }
+    
+
     
     
     
